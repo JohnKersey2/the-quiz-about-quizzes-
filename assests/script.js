6b3712a1c6f65f64439bc028b2b1c389259378b8
John Kersey <middlejohn@yahoo.com>
2022-09-26 13:02:35 -0400

    commented pseudocode needed to continue on with project.

--- assests/script.js
+++ assests/script.js
@@ -1,26 +1,44 @@
-var timer = document.querySelector(".timer");
+var timer = document.querySelector(".timer"); // puts the display timer in the correct place. may not need to global
 
-var score = document.getElementById("score");
+var score = document.getElementById("score"); // may be useless? idk I'm too dumb to figure out this part right now
 
-var secondsLeft = 120;
+var scoreTracker = 0 // increment when answer = true
+
+var currentQuestion = 0 // increment when answer is clicked, tie array of questions and answers to this varriable. 
+
+var secondsLeft = 120; // all running functions wait for button click and decrement every 1000ms
+
+
 
 function quizTimer() {
-    document.getElementById("startButton").style.display = "none";
+    document.getElementById("startButton").style.display = "none"; // hides start button
+    //display question / answer flexboxes - hardcore in first question/answers 
     var timerInterval = setInterval(function() {
       secondsLeft--;
       timer.textContent = "You have " + secondsLeft + " seconds left!";
-  
-      if(secondsLeft === 0) {
+
+      if(secondsLeft === 0) {  // duplicate for if currentQuestion === 10
         clearInterval(timerInterval);
         sendScore();
       }
+
+      
+      // write currentQuestion to <p>'s 
+      // listen for click
+      // on click, function that changes the scoreTracker and currentQuestion, decrement if wrong
+
+
     }, 1000);
   }
+
+  // Ways to get question display? Maybe have a variable track which quesiton you're on, display that. 
 
   // Maybe tie a secondsLeft -15 to wrong answers when clicked. 
 
   function sendScore() {
     timer.textContent = "You scored " + score + " out of 10!";
+    // hide question/answers boxes 
+    // display button that prompts initials / score to be tracked. "Would you like to save your score?"
   }
   
   document.getElementById("startButton").addEventListener ("click", quizTimer);
